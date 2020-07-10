ee62a0b2e584d98b24ce841dca06fd2ff9f3d334
ilyshev <dilyshev@mac.com>
2020-07-10 14:29:11 -0400

    day 2. adding Client and Server

--- test_functions.py
+++ test_functions.py
@@ -10,13 +10,3 @@ def max_for_dicts(elements, key):
             max_element = element
 
     return max_element
-
-
-def filter_dicts(elements, key, min_value):
-    new_elements = []
-
-    for element in elements:
-        if element[key] >= min_value:
-            new_elements.append(element)
-
-    return new_elements
